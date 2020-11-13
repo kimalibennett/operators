72b312d5585eb8160db9c7a45436f714572abf67
kimalibennett <aminbennett@gmail.com>
2020-11-13 00:12:55 -0600

    commit tree f53fc3345254b36abb1c4d7bc9101a0f1822120d

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
 def unsafe?(speed)
-
+if speed > 60 || speed < 40
+    return true 
+elsif speed == 50
+    return false 
 end
-
-
 
 def not_safe?(speed)
-	
+    speed < 40 && speed > 60 ? true : false
+  end
+
 end
-	
-
-
